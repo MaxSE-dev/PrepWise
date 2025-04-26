04114bafb623b12a3bf258778ac1b342cbc5b1b7
killya <maksimsorochinski@192.168.10.5>
2025-04-26 16:19:57 +0500

    ignore eslint and typescript errors (for now)

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -12,11 +12,8 @@ enum CallStatus {
 const Agent = ({ userName }: AgentProps) => {
   const callStatus = CallStatus.FINISHED;
   const isSpeaking = true;
-  const messages = [
-    'Whats your name?',
-    'My name is Max'
-  ]
-  const lastMessage = messages[messages.length - 1]
+  const messages = ["Whats your name?", "My name is Max"];
+  const lastMessage = messages[messages.length - 1];
 
   return (
     <>
@@ -48,16 +45,21 @@ const Agent = ({ userName }: AgentProps) => {
           </div>
         </div>
       </div>
-        {messages.length > 0 && (
-          <div className="transcript-border">
-            <div className="transcript">
-              <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0', 'animate-fadeIn, opacity-100')}>
-                {lastMessage}
-              </p>
-            </div>
+      {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn, opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
           </div>
-        )}
-
+        </div>
+      )}
 
       <div className="w-full flex justify-center">
         {callStatus !== "ACTIVE" ? (
